eb57228c682b0ae83ca2009b8d044f7242afb144
a1dnan <aaliladnane@gmail.com>
2023-05-22 23:51:44 +0100

    Add Project files

---
+++ config-server/src/test/java/com/a1dnan/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1dnan.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
